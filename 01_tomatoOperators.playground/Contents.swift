702572eec1d7d41cf2a3ff537267f6e22c441493
고소담 <kosodam@gosodam-ui-MacBook-Air.local>
2017-03-12 23:43:11 +0900

    ADD basic operators

---
+++ 01_tomatoOperators.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var number1 = 1
+
+str
+
+number1
+
+let number2 = 2
+
+number1 = 2
+
+//number2 = 4
+1 + 2
+5-4
+10.0/2.5
+2*3
+
+//5/0
+
+//str = 5
+
+str = "tomato"
+
+5%2
+
+number1 += 2
+
+//numbaer1 + 2 =
+
+"tomato " + "juice"
+
